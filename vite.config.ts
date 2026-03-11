fa5277e7d4279b3ea5485d03a53d09f3e7803194
Tomas Aprile <tomas.eaprile@gmail.com>
2026-03-11 17:34:40 -0300

    feat(release): update release pipeline and configuration for semantic release

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'node:path';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  build: {
+    emptyOutDir: false,
+    minify: mode === 'production' ? 'esbuild' : false,
+    sourcemap: mode === 'development',
+    lib: {
+      entry: resolve(process.cwd(), 'src/index.ts'),
+      name: 'Navigation',
+      fileName: () => 'navigation.browser.js',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['gsap'],
+    },
+  },
   test: {
-    environment: 'node',
+    environment: 'jsdom',
     globals: true,
   },
-});
+}));
